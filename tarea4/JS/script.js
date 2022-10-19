a42e509654d432ddd4bdd6f869baff305e0f0847
Marcello <110632107+MMelofiro@users.noreply.github.com>
2022-10-18 19:34:34 -0500

    Add files via upload

---
+++ tarea4/JS/script.js
@@ -0,0 +1,59 @@
+/* Menú hamburguesa */
+function llamar(){
+    let botonera=document.getElementById("botonera");
+        botonera.classList.toggle("botonera-open");
+    let menu=document.getElementById("menu");
+        menu.querySelector("#menu .fa-bars").classList.toggle("fa-times");
+    let noscroll=document.getElementById("cuerpo");
+        noscroll.classList.toggle("cuerpofixed");
+    let barracambio=document.getElementById("barra");
+        barracambio.classList.toggle("header-cambio");
+}
+
+/* Cambio de color header */
+window.addEventListener("scroll", colorbarra);
+function colorbarra(){
+    let barrasup=document.getElementById("barra");
+        posy=window.pageYOffset;
+    if(posy>300){
+        barrasup.classList.remove("cheader");
+        barrasup.classList.add("header-color");
+    }
+    else{
+        barrasup.classList.add("cheader");
+        barrasup.classList.remove("header-color");
+    }
+}
+
+/* Galería */
+let num=1;
+function adelante(){
+    num += 1;
+    if(num>12){
+        num=1;
+    }
+    let img=document.getElementById("imagen");
+        img.src="imagenes/"+"img0"+num+".jpg";
+}
+
+function atras(){
+    num -= 1;
+    if(num<1){
+        num=12;
+    }
+    let img=document.getElementById("imagen");
+        img.src="imagenes/"+"img0"+num+".jpg";
+}
+
+/* Botón subir */
+window.addEventListener("scroll", botonSubir)
+function botonSubir(){
+    let subir=document.getElementById("subir");
+        posy=window.pageYOffset;
+    if(posy>350){
+        subir.style.display="block"
+    }
+    else{
+        subir.style.display="none"
+    }
+}
